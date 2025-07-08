571bee150736a4a56ed857deb2f021e466028a2b
Tobia Marcucci <tobia.marcucci@gmail.com>
2025-07-08 14:35:24 -0700

    Cleans spanning tree example

--- examples/spanning_tree.py
+++ examples/spanning_tree.py
@@ -1,5 +1,6 @@
 import cvxpy as cp
 import numpy as np
+from itertools import product
 from gcspy import GraphOfConvexPrograms
 
 # initialize empty graph
@@ -8,28 +9,25 @@ graph = GraphOfConvexPrograms()
 # create vertices on a grid
 grid_side = 3
 radius = .25
-for i in range(grid_side):
-    for j in range(grid_side):
-        v = graph.add_vertex((i, j))
-        x = v.add_variable(2)
-        center = np.array([i, j])
-        v.add_constraint(cp.norm2(x - center) <= radius)
+grid_points = lambda dim: product(*[range(grid_side)] * dim)
+for i, j in grid_points(2):
+    v = graph.add_vertex((i, j))
+    x = v.add_variable(2)
+    center = np.array([i, j])
+    v.add_constraint(cp.norm2(x - center) <= radius)
 
 # add edges between neighboring vertices
-for i in range(grid_side):
-    for j in range(grid_side):
-        for k in range(grid_side):
-            for l in range(grid_side):
-                distance = abs(k - i) + abs(l - j)
-                if distance > 0 and distance <= 1:
-                    tail = graph.get_vertex((i, j))
-                    head = graph.get_vertex((k, l))
-                    edge = graph.add_edge(tail, head)
+for i, j, k, l in grid_points(4):
+    distance = abs(k - i) + abs(l - j)
+    if distance > 0 and distance <= 1:
+        tail = graph.get_vertex((i, j))
+        head = graph.get_vertex((k, l))
+        edge = graph.add_edge(tail, head)
 
-                    # edge cost is Euclidean distance
-                    x_tail = tail.variables[0]
-                    x_head = head.variables[0]
-                    edge.add_cost(cp.norm2(x_head - x_tail))
+        # edge cost is Euclidean distance
+        x_tail = tail.variables[0]
+        x_head = head.variables[0]
+        edge.add_cost(cp.norm2(x_head - x_tail))
 
 # run followin code only if this file is executed directly, and not when it is
 # imported by other files
