63da241733ce40d69aa6bbc8b724266a4aff7fea
Tobia Marcucci <tobia.marcucci@gmail.com>
2025-07-09 14:27:38 -0700

    Restores circle cover example.

--- examples/facility_location/facility_location.py
+++ examples/facility_location/facility_location.py
@@ -7,44 +7,44 @@ graph = GraphOfConvexPrograms()
 
 # facility 1
 f1 = graph.add_vertex("f1")
-xf1 = f1.add_variable(2)
-cf1 = np.array([0, 2.5])
-f1.add_constraint(cp.norm_inf(xf1 - cf1) <= .5)
+x = f1.add_variable(2)
+c = np.array([0, 2.5])
+f1.add_constraint(cp.norm_inf(x - c) <= .5)
 
 # facility 2
 f2 = graph.add_vertex("f2")
-xf2 = f2.add_variable(2)
-cf2 = np.array([0, -1])
-Df2 = np.diag([2, .5])
-f2.add_constraint(cp.norm_inf(Df2 @ (xf2 - cf2)) <= 1)
+x = f2.add_variable(2)
+c = np.array([0, -1])
+D = np.diag([2, .5])
+f2.add_constraint(cp.norm_inf(D @ (x - c)) <= 1)
 
-# user 1
-u1 = graph.add_vertex("u1")
-xu1 = u1.add_variable(2)
-cu1 = np.array([3, .5])
-u1.add_constraint(cp.norm2(xu1 - cu1) <= .5)
+# client 1
+c1 = graph.add_vertex("c1")
+x = c1.add_variable(2)
+c = np.array([3, .5])
+c1.add_constraint(cp.norm2(x - c) <= .5)
 
-# user 2
-u2 = graph.add_vertex("u2")
-xu2 = u2.add_variable(2)
-cu2 = np.array([3, -1])
-u2.add_constraint(cp.norm2(xu2 - cu2) <= .5)
+# client 2
+c2 = graph.add_vertex("c2")
+x = c2.add_variable(2)
+c = np.array([3, -1])
+c2.add_constraint(cp.norm2(x - c) <= .5)
 
-# user 3
-u3 = graph.add_vertex("u3")
-xu3 = u3.add_variable(2)
-cu3 = np.array([3, -2.5])
-u3.add_constraint(cp.norm2(xu3 - cu3) <= .5)
+# client 3
+c3 = graph.add_vertex("c3")
+x = c3.add_variable(2)
+c = np.array([3, -2.5])
+c3.add_constraint(cp.norm2(x - c) <= .5)
 
 # add edges
 facilities = [f1, f2]
-users = [u1, u2, u3]
+clients = [c1, c2, c3]
 for facility in facilities:
-    for user in users:
-        edge = graph.add_edge(facility, user)
+    for client in clients:
+        edge = graph.add_edge(facility, client)
         x_facility = facility.variables[0]
-        x_user = user.variables[0]
-        edge.add_cost(cp.norm2(x_user - x_facility))
+        x_client = client.variables[0]
+        edge.add_cost(cp.norm2(x_client - x_facility))
 
 # run followin code only if this file is executed directly, and not when it is
 # imported by other files
