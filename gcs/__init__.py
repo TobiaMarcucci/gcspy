bd03bd198959f2cad6b401ea626f47b01bc8dda1
Tobia Marcucci <tobia.marcucci@gmail.com>
2025-08-22 16:43:11 -0700

    Fixes a few things.

--- gcs/__init__.py
+++ gcs/__init__.py
@@ -1,1 +1,4 @@
+__version__ = "0.1.0"
+__author__ = "Tobia Marcucci"
+
 from gcs.graphs import GraphOfConicSets, GraphOfConvexSets
--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='gcs',
-    version='0.0.1',
+    name="gcs",
+    version="0.1.0",
     description="Library for solving optimization problems over Graphs of Convex Sets (GCS).",
     author="Tobia Marcucci",
+    author_email='marcucci@ucsb.edu',
+    license='MIT',
     packages=find_packages(),
     install_requires=[
         "numpy",
