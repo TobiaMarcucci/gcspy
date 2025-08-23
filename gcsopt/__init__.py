0139a9e601a09b0275b93a80938e2cdf911e57a9
Tobia Marcucci <tobia.marcucci@gmail.com>
2025-08-23 12:34:28 -0700

    Updates setup.py and version

--- gcsopt/__init__.py
+++ gcsopt/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Tobia Marcucci"
 
 from gcsopt.graphs import GraphOfConicSets, GraphOfConvexSets
--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="gcsopt",
-    version="0.1.0",
+    version="0.1.1",
     description="Library for solving optimization problems over Graphs of Convex Sets (GCS).",
+    long_description=long_description,
     author="Tobia Marcucci",
     author_email='marcucci@ucsb.edu',
     license='MIT',
